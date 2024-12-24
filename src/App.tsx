ccfbe402a4b1aba80ecdbf77614f14461382e360
Ana Carolina <anacarolinaneias@gmail.com>
2024-12-24 09:21:32 -0300

    Adicionando pastas e elementos

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,52 @@
 import React from "react";
-import {View, Text} from "react-native"
+import { View, Text } from "react-native"
+import AppLoading from "expo-app-loading"
+import {ThemeProvider} from "styled-components/native"
 
-const App: React.FC = () => {
+
+import {
+    useFonts,
+    Poppins_300Light,
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+    Poppins_800ExtraBold
+} from "@expo-google-fonts/poppins"
+
+import { DMSans_400Regular } from "@expo-google-fonts/dm-sans"
+import { DMSerifDisplay_400Regular } from "@expo-google-fonts/dm-serif-display"
+
+import COLORS from "../src/styles/theme"
+
+export default function App() {
+    const [fontsLoaded] = useFonts({
+        Poppins_300Light,
+        Poppins_400Regular,
+        Poppins_500Medium,
+        Poppins_700Bold,
+        Poppins_800ExtraBold,
+        DMSans_400Regular,
+        DMSerifDisplay_400Regular,
+    })
+
+    if(!fontsLoaded){
+        return <AppLoading />
+    }
+
     return (
-        <View>
-            <Text>Wallet App</Text>
+        <ThemeProvider theme={COLORS}>
+        <View
+            style={
+                {
+                    flex: 1,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }
+            }
+        >
+            <Text>Carteira App</Text>
         </View>
-    )
+        </ThemeProvider>
+    );
 }
 
-export {App};
