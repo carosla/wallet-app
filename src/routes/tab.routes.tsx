1c7c9785de509b0cc84ae0ff1a803d66e4262956
carosla <anacarolinaneias@gmail.com>
2025-02-17 00:37:20 -0300

    Criação e ajustes na tabbar

--- src/routes/tab.routes.tsx
+++ src/routes/tab.routes.tsx
@@ -1,10 +1,75 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { View } from 'react-native'
+import {CreditCard} from 'phosphor-react-native'
 
-export const tabRoutes = () => {
+import theme from '../styles/theme'
+import { Carteira } from '../screens/Tab/Carteira'
+import { Notificacao } from '../screens/Tab/Notificacao'
+import { Relatorio } from '../screens/Tab/Relatorio'
+import { Settings } from '../screens/Tab/Settings'
+
+const {Navigator, Screen} = createBottomTabNavigator();
+
+export const TabRoutes = () => {
   return (
-    <View>
-      <Text>tab routes</Text>
-    </View>
+    <Navigator
+      initialRouteName='Carteira'
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: true,
+        tabBarInactiveTintColor: theme.COLORS.GRAY4,
+        tabBarActiveTintColor: theme.COLORS.GRAY1,
+        tabBarStyle: {
+          backgroundColor: theme.COLORS.GRAY6,
+        },
+        tabBarLabelStyle: {
+          fontSize: 11,
+          color: theme.COLORS.GRAY3
+        }
+      }}
+    >
+      <Screen 
+        name='Carteira' 
+        component={Carteira}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ focused }) => (
+            <View
+            style={{
+              alignItems: 'center',
+              justifyContent: 'center',
+          }}
+            >
+              {focused ? (
+              <CreditCard 
+                size={25} 
+                weight='light'
+                color={focused ? theme.COLORS.GRAY1 : theme.COLORS.GRAY3}
+              />
+              ) : (
+                <CreditCard 
+                size={25} 
+                weight='fill'
+                color={focused ? theme.COLORS.GRAY1 : theme.COLORS.GRAY3}
+              />
+              )}
+            </View>
+          )
+        }}
+      />
+      <Screen 
+        name='Notificacao' 
+        component={Notificacao}
+      />
+      <Screen 
+        name='Relatorio' 
+        component={Relatorio}
+      />
+      <Screen 
+        name='Settings' 
+        component={Settings}
+      />
+    </Navigator>
   )
 }
